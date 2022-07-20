65aaf41b50db6002569070a72478d82235a16d06
Pedro Teixeira <pedroteixeira091@gmail.com>
2022-07-20 11:15:11 +0200

    First commit of all - commiting all files to the repository

---
+++ script.js
@@ -0,0 +1,20 @@
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number"));
+}
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum)
+
+let guess = parseInt(prompt("Enter your first guess"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess")
+    } else {
+        guess = prompt('Too low! Enter a new guess')
+    }
+}
+console.log(`It took you ${attempts} attempts to get it right. The correct number was ${guess}`)
